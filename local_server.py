97368a9b49792441e04251f7292c514ff0fd9be3
Artrix <artrix@126.com>
2017-08-03 13:32:57 +0800

    Added the remote server module

--- local_server.py
+++ local_server.py
@@ -33,84 +33,97 @@ def new_request_process(source_socks, source_address):
 
         is_exit = False
 
+        received = None
         try:
             received = source_socks.recv(128 * 1024)
         except OSError:
             pass
         time.sleep(1)
 
-        try:
-            decoded_data = received.decode("gb2312")
-        except UnicodeDecodeError:
+        if received:
+            buf = None
             try:
-                decoded_data = received.decode("utf-8")
+                buf = received.decode("gb2312")
             except UnicodeDecodeError:
-                decoded_data = "exit"
-                is_exit = True
+                try:
+                    buf = received.decode("utf-8")
+                except UnicodeDecodeError:
+                    buf = None
+                    is_exit = True
 
-        if not received or decoded_data == 'exit':
-            source_socks.close()
-            break
-        buf = received.decode("gb2312")
+            if buf == 'exit' or not buf:
+                source_socks.close()
+                break
 
-        if buf and "CONNECT" not in buf and not is_exit:
+            if buf and "CONNECT" not in buf and not is_exit:
 
-            try:
-                target_header = buf.split("\r\n")
-                target_host = target_header[1].split(":")[1].strip()
-            except IndexError:
-                pass
+                try:
+                    target_header = buf.split("\r\n")
+                    target_host = target_header[1].split(":")[1].strip()
+                except IndexError:
+                    pass
 
-            port = 80
-            try:
-                cut_result = cut_string(buf, target_host + ":", "/")
-                if cut_result.isdigit():
-                    port = int(cut_result)
-            except ValueError:
-                pass
-            finally:
-                if IS_DEBUG:
-                    print("Thr:" + target_host + "  Port=" + str(port))
+                port = 80
+                try:
+                    cut_result = cut_string(buf, target_host + ":", "/")
+                    if cut_result.isdigit():
+                        port = int(cut_result)
+                except ValueError:
+                    pass
+                finally:
+                    if IS_DEBUG:
+                        print("Thr:" + target_host + "  Port=" + str(port))
 
-            print("Target Host:" + target_host + ":" + str(port))
+                print("Target Host:" + target_host + ":" + str(port))
 
-            connect_succeed = False
-            target_host_address = (target_host, port)
-            target_socks = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            try:
-                target_socks.connect(target_host_address)
-                connect_succeed = True
-            except TimeoutError:
+                connect_succeed = False
+                target_host_address = (target_host, port)
+                target_socks = socket.socket(
+                    socket.AF_INET, socket.SOCK_STREAM)
                 try:
                     target_socks.connect(target_host_address)
                     connect_succeed = True
                 except TimeoutError:
-                    target_socks.close()
-            except ConnectionRefusedError:
-                try:
-                    target_socks.connect(target_host_address)
-                    connect_succeed = True
+                    try:
+                        target_socks.connect(target_host_address)
+                        connect_succeed = True
+                    except TimeoutError:
+                        target_socks.close()
                 except ConnectionRefusedError:
-                    target_socks.close()
+                    try:
+                        target_socks.connect(target_host_address)
+                        connect_succeed = True
+                    except ConnectionRefusedError:
+                        target_socks.close()
+                except socket.gaierror:
+                    try:
+                        target_socks.connect(target_host_address)
+                        connect_succeed = True
+                    except socket.gaierror:
+                        target_socks.close()
 
-            if connect_succeed:
-                # send the source request
-                target_socks.send(received)
+                if connect_succeed:
+                    # send the source request
+                    target_socks.send(received)
 
-                # get data for 128bits per batch while the data is still
-                # sending.
-                try:
-                    target_socks.settimeout(0.5)
-                    data = target_socks.recv(128)
-                    while data:
-                        source_socks.send(data)
+                    # get data for 128bits per batch while the data is still
+                    # sending.
+                    try:
+                        target_socks.settimeout(0.5)
                         data = target_socks.recv(128)
-                except socket.timeout:
-                    target_socks.close()
-                except ConnectionAbortedError:
-                    target_socks.close()
-        else:
-            source_socks.close()
+                        while data:
+                            source_socks.send(data)
+                            data = target_socks.recv(128)
+                    except socket.timeout:
+                        target_socks.close()
+                    except ConnectionAbortedError:
+                        target_socks.close()
+                    except OSError:
+                        target_socks.close()
+
+            else:
+                source_socks.close()
+                break
 
 
 proxy_server_address = ('127.0.0.1', 2850)
